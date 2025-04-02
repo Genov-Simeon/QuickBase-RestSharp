6d8d9a6e0c46b0528095a6d9b4902bfaf3dfa54b
sgenov <genov.simeon86@googlemail.com>
2025-04-03 02:38:49 +0300

    remove constants

--- QuickBaseApi.Client/Constants/QuickBaseConstants.cs
+++ QuickBaseApi.Client/Constants/QuickBaseConstants.cs
@@ -1,8 +1,5 @@
 public static class QuickBaseConstants
 {
-    public const string DefaultBaseUrl = "https://api.quickbase.com/v1";
-    public const int DefaultTimeout = 30000; // 30 seconds
-
     public static class Headers
     {
         public const string RealmHostname = "QB-Realm-Hostname";
